a2466bb509b8feee4d6bcdda3ea1daec199b3bc0
Gorbunov Kirill <sopr54@yandex.ru>
2023-11-29 08:30:25 +0700

    fix: изменить ендпоинт

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,4 @@
-export const SERVER_URL: string = 'http://127.0.0.1:1337'
+export const SERVER_URL: string =
+  'http://84.201.142.203:1337'
 export const URL_ENDPOINT: string =
   'http://84.201.142.203:1337/api/cards?populate=*'
